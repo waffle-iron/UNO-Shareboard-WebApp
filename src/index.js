2a12234ce9ab0c8ca5fbad6776bb3daa2e2c6680
Parker <psprouse@uno.edu>
2017-02-13 14:44:12 -0600

    Added a library for parsing phone numbers; started working on the verification form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Landing from './js/pages/LandingPage';
+import UserVerificationPage from './js/pages/UserVerificationPage';
 import Home from './js/pages/HomePage';
 import Advertisements from './js/pages/AdvertisementListPage';
 import AdvertisementDetails from './js/pages/AdvertisementDetailsPage';
@@ -11,6 +12,7 @@ import './css/styles.css';
 ReactDOM.render((
   <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
     <Route path="/" component={Landing} />
+    <Route path="verify" component={UserVerificationPage} />
     <Route path="home" component={Home} />
     <Route path="advertisements" component={Advertisements} />
     <Route path="advertisements/:id" component={AdvertisementDetails} />
--- src/js/utility/utilities.js
+++ src/js/utility/utilities.js
@@ -1,9 +1,20 @@
+var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
 module.exports = {
 
   validateEmail: function(email) {
     // eslint-disable-next-line
     const re = /^[A-z0-9_%+-]+.*[A-z0-9_%+-]+@(my.)?uno.edu$/;
     return re.test(email);
+  },
+
+  validatePhone: function(number) {
+    try {
+      return phoneUtil.parse(number, 'US');
+    }
+    catch (err) {
+      return false;
+    }
   }
 
 }
